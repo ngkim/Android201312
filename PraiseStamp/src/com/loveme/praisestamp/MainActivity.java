756c4063da179305448a9b5019556a2440421a7a
Namgon Kim <ng.kim@kt.com>
2013-12-25 12:41:23 +0900

    Define StampView as TextView

--- PraiseStamp/src/com/loveme/praisestamp/MainActivity.java
+++ PraiseStamp/src/com/loveme/praisestamp/MainActivity.java
@@ -28,86 +28,10 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	private int stampsPerRow = 4;
+	private int stampsPerRow = 3;
 	private DtoFactory dtoFactory;
 	private PraiseStamp pStamp;
-	
-	private ImageView goodJobStamp() {
-		// day 1 column
-		ImageView imgStamp = new ImageView(this);
-		imgStamp.setImageDrawable(getResources()
-				.getDrawable(R.drawable.goodjob));
-		imgStamp.setPadding(0, 0, 0, 0); // padding in each image if needed
-
-		return imgStamp;
-	}
-	
-	private void putGoodJobStamp(View v) {
-		ImageView imgStamp = (ImageView)v;
-    	imgStamp.setImageDrawable(getResources()
-				.getDrawable(R.drawable.goodjob));
-    	pStamp.increaseNowCnt();
-    	Toast.makeText(MainActivity.this,
-                "Good Job!!!",
-                Toast.LENGTH_LONG).show();
-	}
-	
-	public void delete(View v) {
-		ImageView imgStamp = (ImageView)v;
-		imgStamp.setImageDrawable(getResources()
-				.getDrawable(R.drawable.empty));
-		pStamp.increaseNowCnt();
-		Toast.makeText(MainActivity.this,
-                "Clear Stamp!!!",
-                Toast.LENGTH_LONG).show();
-	}
-	
-	private void removeGoodJobStamp(View v) {
-    	DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-    	    @Override
-    	    public void onClick(DialogInterface dialog, int which) {
-    	        switch (which){
-    	        case DialogInterface.BUTTON_POSITIVE:
-    	        	
-    	        	dialog.dismiss();
-    	            break;
-
-    	        case DialogInterface.BUTTON_NEGATIVE:
-    	            dialog.dismiss();
-    	            break;
-    	        }
-    	    }
-    	};
-
-    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    	builder.setMessage(R.string.delete_stamp).setPositiveButton(R.string.str_yes, dialogClickListener)
-    	    .setNegativeButton(R.string.str_no, dialogClickListener).show();
-    	
-    	
-    	
-	}
-
-	private ImageView emptyStamp() {
-		ImageView imgStamp = new ImageView(this);
-		imgStamp.setImageDrawable(getResources().getDrawable(R.drawable.empty));
-		imgStamp.setPadding(0, 0, 0, 0); // padding in each image if needed
-		imgStamp.setOnClickListener(new View.OnClickListener() {
-		    @Override
-		    public void onClick(View v) {
-		    	putGoodJobStamp(v);	        
-		    }
-		});
-		imgStamp.setOnLongClickListener(new View.OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				removeGoodJobStamp(v);
-				return false;
-			}
-		});
-
-		return imgStamp;
-	}
-
+		
 	private void createTables() {
 //		PraiseStamp stamp = getStampFromDB();
 		
@@ -143,11 +67,11 @@ public class MainActivity extends Activity {
 			for (int j = 0; j < stampsPerRow; j++) {
 				int curPos = i * stampsPerRow + j;
 				if (curPos < pStamp.getNowCnt())
-					row.addView(goodJobStamp());
+					row.addView(new StampView(this, "12/25"));
 				else if (curPos >= pStamp.getGoalCnt())
 					break;
 				else
-					row.addView(emptyStamp());
+					row.addView(new StampView(this));
 			}
 		}
 
